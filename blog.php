163d29cceb251fcd5095578242e0e5edf8c6bdbc
Edgar Ramirez <edgarramirez2173@gmail.com>
2014-11-03 13:27:36 -0800

    header and footer

--- blog.php
+++ blog.php
@@ -5,7 +5,9 @@
 </head>
 <body>
 <?php 
+      require_once(__DIR__ . "/view/header.php");
       require_once(__DIR__ . "/controller/create-db.php");
+      require_once(__DIR__ . "/view/footer.php");
 ?>
 </body>
 </html>
