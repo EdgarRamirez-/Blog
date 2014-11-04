1c2cd36d980c2aca8c252c5ee7a03c91774a7166
Edgar Ramirez <edgarramirez2173@gmail.com>
2014-11-04 12:11:13 -0800

    navigation,config,post

--- blog.php
+++ blog.php
@@ -8,6 +8,7 @@
       require_once(__DIR__ . "/view/header.php");
       require_once(__DIR__ . "/controller/create-db.php");
       require_once(__DIR__ . "/view/footer.php");
+      require_once(__DIR__ . "/view/navigation.php");
 ?>
 </body>
 </html>
