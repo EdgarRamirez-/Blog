b0c360b27498742db9a128dcaacace7f70c2b9d1
Edgar Ramirez <edgarramirez2173@gmail.com>
2014-11-05 12:15:15 -0800

    css

--- view/form.php
+++ view/form.php
@@ -1,12 +1,16 @@
 <h1>Create Blog Post</h1>
-
+/
 <form>
+   <!-- this for title -->
+   <!-- label-text that displayed in front of the text line -->
+   <!-- label is for title -->
      <div>
          <label for="title">Title </label>
          <input type="text" name="title"/>
     </div>
-    
-
+  <!-- holds our info for our post -->
+  <!-- textarea lets us type as many sentences as we can -->
+  <!-- spans up more than one line -->
     <div>
          <label for="post">Post: </label>
          <textarea name="post"></textarea>
