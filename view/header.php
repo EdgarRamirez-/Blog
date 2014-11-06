fe541a1f8fa31329098b8c6062b25a391c774eda
Edgar Ramirez <edgarramirez2173@gmail.com>
2014-11-06 12:08:15 -0800

    media query and css

--- view/header.php
+++ view/header.php
@@ -1,6 +1,9 @@
 <html>
   <head>
-     <meta charset='UTF-8'>
+     <link rel="stylesheet" type="text/css" href="main2.css">
+    <meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> 
+
+     <meta charset=UTF-8'>
       <title> </title>
       </head>
       <body>
