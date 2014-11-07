944a114c00065220cccf01e9373a701b016d4451
Edgar Ramirez <edgarramirez2173@gmail.com>
2014-11-07 11:15:15 -0800

    commented on create-post.php

--- create-post.php
+++ create-post.php
@@ -1,4 +1,6 @@
 <?php
+ //*  two variables that submit data
+//* filter validates sanitizes t make sure there are no mailicios things
   $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
   $post = filter_input(INPUT_POST,"post", FILTER_SANITIZE_STRING );
 
