dfbb124c86cdebecd84d98888be9e49a052beaa4
Edgar Ramirez <edgarramirez2173@gmail.com>
2014-11-12 12:14:05 -0800

    blog video 22

--- view/form.php
+++ view/form.php
@@ -1,6 +1,9 @@
+<?php 
+   require_once(__DIR__ . "/../model/config.php");
+?>
 <h1>Create Blog Post</h1>
-/
-<form>
+
+<form method="post" action="<?php echo $path . "controller/create-post.php"; ?>">
    <!-- this for title -->
    <!-- label-text that displayed in front of the text line -->
    <!-- label is for title -->
