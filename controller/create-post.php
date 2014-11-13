db72242157c1650932b25ce110834b67329f5760
Edgar Ramirez <edgarramirez2173@gmail.com>
2014-11-13 12:07:40 -0800

    php video 24
     made a query and else if statements

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,8 +1,23 @@
 <?php
  //*  two variables that submit data
 //* filter validates sanitizes t make sure there are no mailicios things
+  require_once(__DIR__ . "/../model/database.php");
+
+  $connection = new mysqli($host, $username, $password, $database);
+
   $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
   $post = filter_input(INPUT_POST,"post", FILTER_SANITIZE_STRING );
+// add new records into your MYSQLtable
+  $query = $connection->query("INSERT INTO  posts SET title = '$title', post = '$post'");
 
+   if ($query) {
+    echo "<p>Successfully inserted post: $title</p>";   	
+   } else {
+   	   echo "<p>$connection->error</p>";
+   }
+   
+   
    echo "<p>Title: $title</p>";
    echo "<p>Post: $post</p>";
+
+   $connection->close();
