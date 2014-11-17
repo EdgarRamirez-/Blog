cdb53c0a0d250fe361f29161509a0523833e891c
Edgar Ramirez <edgarramirez2173@gmail.com>
2014-11-17 13:31:43 -0800

    blog video 28
    commented of what the construct means

--- model/database.php
+++ model/database.php
@@ -6,7 +6,24 @@ class Database {
 	private $password;
 	private $database;
 
-    public functionconstruct ($host, $username, $password, $database) {
-          $this->host
+    public function__construct ($host, $username, $password, $database) {
+          $this->host = $host;
+          $this->username = $username;
+          $this->password = $password;
+          $this->database = $database;
     }
-}
+   
+    public function openConnection() {
+
+    }
+    
+    public function closeConnection() {
+
+    }
+   
+    public function query($string) {
+
+    }
+ }
+//* The constructor is called on an object after it has been created,
+//* and is a good place to put initialisation code.
